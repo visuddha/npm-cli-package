326d5bc09119df9275576d6aae2b9bd82fafe6e3
Visuddha Karunaratne <visuddha@gmail.com>
2023-08-18 12:40:22 +0530

    adding inquirer to prompting

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 
 const yargs = require("yargs");
 const {argv} = yargs(process.argv);
+const inquirer = require("inquirer");
+
+const prompt = inquirer.createPromptModule()
+prompt([
+    {
+        type: "input",
+        name : "pokemon",
+        message : "Enter a pokemon name to view its moves"
+    }
+]).then((answer) => {
+    const pokemon = answer.pokemon;
+    printMoves(pokemon);
+})
 
 const printMoves =  (async (charater) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${charater}`);
@@ -9,5 +22,3 @@ const printMoves =  (async (charater) => {
     const moves = pokmon.moves.map(({move}) => move.name)
     console.log(moves.slice(0,5));
 });
-    
-printMoves(argv.pokemon)
