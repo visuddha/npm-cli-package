e9149628ee864f06bdcad9dd6bfd0583ef538175
Visuddha Karunaratne <visuddha@gmail.com>
2023-08-18 11:28:21 +0530

    inital commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("demo cli")
+#!/usr/bin/env node
+
+const yargs = require("yargs");
+const {argv} = yargs(process.argv);
+
+const printMoves =  (async (charater) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${charater}`);
+    const pokmon = await response.json();
+    const moves = pokmon.moves.map(({move}) => move.name)
+    console.log(moves.slice(0,5));
+});
+    
+printMoves(argv.pokemon)
